d25372ed788dcbaada12bcf28e63f1717b62dbcd
Ntwali-Josue <kntwalijos@gmail.com>
2021-10-12 02:26:01 +0200

    Add Header section

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BsArrowRightCircle } from 'react-icons/bs';
+
+const Header = () => (
+  <>
+    <div className="header">
+      <p>Covid Statistics</p>
+      <BsArrowRightCircle />
+    </div>
+  </>
+);
+
+export default Header;
