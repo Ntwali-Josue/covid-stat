5006551def56e8b53f93d349d073921500af6320
Ntwali-Josue <kntwalijos@gmail.com>
2021-10-12 23:52:11 +0200

    Render the fetched continent and pass values as props

--- src/components/Continent.js
+++ src/components/Continent.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchStats } from '../redux/stats/statsReducer';
+import Home from './Home';
 
-const Continent = () => (
-  <div>
-    <h1>Continent page coming soon</h1>
-  </div>
-);
+const Continent = () => {
+  const dispatch = useDispatch();
+  const covidStats = useSelector((state) => state.stats.statsList);
+  const status = useSelector((state) => state.stats.status);
+  useEffect(() => {
+    if (status === 'empty') {
+      dispatch(fetchStats());
+    }
+  }, [status]);
+
+  const allStats = covidStats.map((stat) => (
+    <Home key={stat.deaths} continent={stat.continent} deaths={stat.deaths} />
+  ));
+
+  return (
+    <div>
+      {allStats}
+    </div>
+  );
+};
 
 export default Continent;
