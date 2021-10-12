f658b3c298c0dde703406f45b234e80d5e5debc1
Ntwali-Josue <kntwalijos@gmail.com>
2021-10-12 10:08:18 +0200

    Configure Routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import {
+  BrowserRouter as Router, Switch, Route,
+} from 'react-router-dom';
 import NavBar from './components/NavBar';
-import Header from './components/Header';
+import Home from './components/Home';
+import Continent from './components/Continent';
 import './styles/App.css';
 
 const App = () => (
   <div className="App">
-    <NavBar />
-    <Header />
+    <Router>
+      <NavBar />
+      <Home />
+      <Switch>
+        <Route path="/continent-1">
+          <Continent />
+        </Route>
+      </Switch>
+    </Router>
   </div>
 );
 
