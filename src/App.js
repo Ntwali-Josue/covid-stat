0b67f5d0341405c40394f012b56108086d885ac4
Ntwali-Josue <kntwalijos@gmail.com>
2021-10-12 02:26:28 +0200

    Render the app.js component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import NavBar from './components/NavBar';
+import Header from './components/Header';
 import './styles/App.css';
 
 const App = () => (
   <div className="App">
     <NavBar />
+    <Header />
   </div>
 );
 
