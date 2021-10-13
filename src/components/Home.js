a95bb7193d44acf20d88e02cb57758f1f4b66914
Ntwali-Josue <kntwalijos@gmail.com>
2021-10-13 14:41:14 +0200

    Remove the dummy div section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,50 +1,13 @@
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { BsArrowRightCircle } from 'react-icons/bs';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = (props) => {
-  const { continent, deaths } = props;
+  const { continent } = props;
 
   return (
     <div>
-      <div className="home-header">
-        <p>Covid Stats</p>
-        <Link to="/continent-1" className="links">
-          <BsArrowRightCircle />
-        </Link>
-      </div>
-      <span className="all-stats">ALL STATS</span>
-      <div className="card">
-        <div className="stats-summary">
-          <div className="sum1">
-            {continent}
-            {deaths}
-            deaths
-          </div>
-          <div className="sum1">
-            {continent}
-            {deaths}
-            recovered
-          </div>
-          <div className="sum1">
-            1302 deaths
-          </div>
-          <div className="sum1">
-            1302 deaths
-          </div>
-        </div>
-        <div className="continents">
-          <div className="continent">
-            <Link to="/continent-1" className="links">
-              continent-1
-            </Link>
-          </div>
-          <div className="continent">
-            <Link to="/continent-1" className="links">
-              continent-1
-            </Link>
-          </div>
-        </div>
+      <div className="card-item">
+        {continent}
       </div>
     </div>
   );
@@ -52,7 +15,6 @@ const Home = (props) => {
 
 Home.propTypes = {
   continent: PropTypes.string.isRequired,
-  deaths: PropTypes.number.isRequired,
 };
 
 export default Home;
